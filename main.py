497ea2e0b7d78180f8433ef3208e06c044cacb83
Jared Godar <jared.godar@gmail.com>
2022-09-22 13:57:25 +0000

    Day 2 complete

---
+++ main.py
@@ -0,0 +1,18 @@
+print('Getting to know you')
+print()
+myName = input('What is your name? ')
+myFood = input('What is your favorite food? ')
+myMusic = input('What is your favorite music? ')
+myState = input('Where do you live? ')
+print()
+print(f'''
+You are {myName}.
+You are probably hungry for {myFood}.
+and you're definitely getting your groove on to {myMusic}
+living in the amazing {myState}
+''')
+
+
+#snake_case
+#camelCase
+
